a322fd1c0e90494169bd4e5a2b2849819b378a41
PatriciaDuranM <patduran16@gmail.com>
2025-01-16 12:51:41 +0100

    update

--- server/src/controllers/users.controller.js
+++ server/src/controllers/users.controller.js
@@ -1,4 +1,4 @@
-// const usersController = [];
+// const usersController = {};
 // const fs = require("fs");
 // const path = require("path");
 // const pathFile = path.resolve(__dirname, "../../data/users.json");
